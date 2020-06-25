00d40070b79fbff7236f1be9c383aebed3cdf179
Zibing Zhang <zzhang2022@gmail.com>
2020-06-25 13:39:45 -0400

    Supports only 3.7 and 3.8 officially now

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.7',
